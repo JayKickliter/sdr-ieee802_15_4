1949254edd0a04d1a527bc93859b2548a53d91ee
Jay Kickliter <jay@kickliter.com>
2015-12-24 16:31:11 -0700

    Enhanced DSSS

--- src/radio_dsss.c
+++ src/radio_dsss.c
@@ -2,37 +2,84 @@
 #include <stdint.h>
 #include <limits.h>
 
-static uint16_t const bpsk_map[] = {0x09af, 0x7650};
+static int const      chips_per_symbol_dbpsk = 15;
+static int const      bits_per_symbol_dbpsk  = 1;
+static size_t const   chip_map_length_dbpsk  = 2;
+static uint32_t const chip_mask_dbpsk        = 0x00007fff;
+static uint32_t const chip_map_dbpsk[]       = {0x000009af, 0x00007650};
+static uint8_t const  symbol_map_dbpsk[]     = {0x00, 0x01};
 
-static size_t count_chip_errors(uint32_t mask, uint32_t ref_chips,
-                                uint32_t chips)
+static int const      chips_per_symbol_oqpsk = 32;
+static int const      bits_per_symbol_oqpsk  = 4;
+static size_t const   chip_map_length_oqpsk  = 16;
+static uint32_t const chip_mask_oqpsk        = 0xffffffff;
+static uint32_t const chip_map_oqpsk[]       = {
+    0x6077ae6c, 0x4e077ae6, 0x6ce077ae, 0x66ce077a, 0x2e6ce077, 0x7ae6ce07,
+    0x77ae6ce0, 0x077ae6ce, 0x1f885193, 0x31f88519, 0x131f8851, 0x1931f885,
+    0x51931f88, 0x051931f8, 0x0851931f, 0x78851931};
+static uint8_t const symbol_map_oqpsk[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+                                           0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
+                                           0x0c, 0x0d, 0x0e, 0x0f};
+
+enum radio_status
+radio_dsss_map_chips_to_symbol(struct radio_dsss const * restrict const obj,
+                               uint32_t chips, uint8_t * restrict const symbol)
 {
-    return liquid_count_ones((chips & mask) ^ (ref_chips & mask));
-}
+    enum radio_status status       = radio_status_no_match;
+    unsigned          min_errors   = UINT_MAX;
+    uint8_t           symbol_match = UINT8_MAX;
 
-uint8_t radio_dsss_map_chips_to_symbol(struct radio_dsss const * const obj,
-                                       uint32_t chips)
-{
-    unsigned int min_errors = UINT_MAX;
-    uint8_t      symbol     = INVALID_SYMBOL;
-
-    for (size_t i = 0; i < obj->chip_map.map_length; ++i)
+    for (size_t i = 0; i < obj->chip_map.length; ++i)
     {
-        unsigned int errors = count_chip_errors(obj->chip_map.chip_mask,
-                                                obj->chip_map.chips[i], chips);
+        unsigned errors = count_bit_errors(obj->chip_map.chips[i], chips);
 
         if (errors < min_errors && errors <= obj->chip_error_threshold)
         {
-            symbol = obj->chip_map.symbols[i];
+            status       = radio_status_ok;
+            min_errors   = errors;
+            symbol_match = obj->chip_map.symbols[i];
         }
     }
 
-    return symbol;
+    *symbol = symbol_match;
+
+    return status;
 }
 
 enum radio_status radio_dsss_init(struct radio_dsss * const obj,
                                   int                 chip_error_threshold,
                                   enum radio_mod_type mod_type)
 {
+    switch (mod_type)
+    {
+    case radio_mod_type_dbpsk:
+        if (chip_error_threshold > chips_per_symbol_dbpsk)
+        {
+            return radio_status_invalid_parameter;
+        }
+        obj->chip_error_threshold      = chip_error_threshold;
+        obj->chip_map.chips_per_symbol = chips_per_symbol_dbpsk;
+        obj->chip_map.bits_per_symbol  = bits_per_symbol_dbpsk;
+        obj->chip_map.mask             = chip_mask_dbpsk;
+        obj->chip_map.length           = chip_map_length_dbpsk;
+        obj->chip_map.symbols          = symbol_map_dbpsk;
+        obj->chip_map.chips            = chip_map_dbpsk;
+        break;
+
+    case radio_mod_type_oqpsk:
+        if (chip_error_threshold > chips_per_symbol_oqpsk)
+        {
+            return radio_status_invalid_parameter;
+        }
+        obj->chip_error_threshold      = chip_error_threshold;
+        obj->chip_map.chips_per_symbol = chips_per_symbol_oqpsk;
+        obj->chip_map.bits_per_symbol  = bits_per_symbol_oqpsk;
+        obj->chip_map.mask             = chip_mask_oqpsk;
+        obj->chip_map.length           = chip_map_length_oqpsk;
+        obj->chip_map.symbols          = symbol_map_oqpsk;
+        obj->chip_map.chips            = chip_map_oqpsk;
+        break;
+    }
+
     return radio_status_ok;
 }
--- src/radio_dsss.h
+++ src/radio_dsss.h
@@ -9,12 +9,13 @@
 
 struct radio_chip_map
 {
-    int chips_per_symbol;  /**< 15 for BPSK, 31 for OQPSK */
-    int bits_per_symbol;   /**< The number of bits a chip sequence represents */
-    uint32_t   chip_mask;  /**< Mask for valid bit positions in chip sequence */
-    size_t     map_length; /**< The number of sequences in the map */
-    uint8_t *  symbols;    /**< The bit values for individual symbols */
-    uint32_t * chips;      /**< The chip values */
+    int chips_per_symbol; /**< 15 for BPSK, 32 for OQPSK */
+    int bits_per_symbol;  /**< The number of bits a chip sequence represents */
+    uint32_t mask;   /**< Mask for valid bit positions in chip sequence */
+    size_t   length;      /**< The number of sequences in the map */
+    uint8_t const *  restrict
+                     symbols; /**< The bit values for individual symbols */
+    uint32_t const * restrict chips; /**< The chip values */
 };
 
 /**
@@ -28,8 +29,9 @@ struct radio_dsss
     struct radio_chip_map chip_map;
 };
 
-uint8_t radio_dsss_map_chips_to_symbol(struct radio_dsss const * const obj,
-                                       uint32_t chips);
+enum radio_status
+radio_dsss_map_chips_to_symbol(struct radio_dsss const * restrict const obj,
+                               uint32_t chips, uint8_t * restrict const symbol);
 
 enum radio_status radio_dsss_init(struct radio_dsss * const obj,
                                   int chip_error_threshold,
